cbef21269e53147d4bb237e962725b087f21d42b
Graham Mendick <graham.mendick@gmail.com>
2016-04-21 18:37:16 +0100

    Added test rendering

--- examples/navigation/src/index.js
+++ examples/navigation/src/index.js
@@ -1,5 +1,22 @@
 import {StateNavigator} from 'navigation';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Catalog from './Catalog.js';
 
 var stateNavigator = new StateNavigator([
     {key: 'catalog', route: ''}
 ]);
+
+stateNavigator.states.catalog.navigated = (data) => {
+    ReactDOM.render(
+        <Catalog
+            albums={data.albums}
+            filter={data.filter}
+            band={data.band}
+            sort={data.sort}
+        />,
+        document.getElementById('catalog')
+    );
+}
+
+stateNavigator.start();
