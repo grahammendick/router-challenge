ea5947027b090cdbab4616dc586e6a7689c5f471
Graham Mendick <graham.mendick@gmail.com>
2016-04-22 12:15:02 +0100

    Added data

--- albums.js
+++ albums.js
@@ -1,5 +1,5 @@
 var ALBUMS = [
-    {id: 1, title:'Abbey Road', band:'The Beatles', side1: ['Two of Us', 'Dig a Pony','Across the Universe','I Me Mine','Dig It','Let It Be','Maggie Mae'], side2: ['I\'ve Got a Feeling','One After 909','The Long And Winding Road','For You Blue','Get Back']},
+    {id: 1, title:'Abbey Road', band:'The Beatles', side1: ['Come Together','Something','Maxwell\'s Silver Hammer','Oh! Darling','Octopus\'s Garden','I Want You (She\'s So Heave)'], side2: ['Here Comes the Sun','Because','You Never Give Me Your Money','Sun King','Mean Mr. Mustard','Polythene Pam','She Came in Through the Bathroom Window','Golden Slumbers','Carry That Weight','The End','Her Majesty']},
     {id: 2, title:'Sticky Fingers', band:'The Rolling Stones', side1: ['Brown Sugar','Sway','Wild Horses','Can\'t You Hear Me Knocking','You Gotta Move'], side2: ['Bitch','I Got the Blues','Sister Morphine','Dead Flowers','Moonlight Mile']},
-    {id: 3, title:'Let It Be', band:'The Beatles'}
+    {id: 3, title:'Let It Be', band:'The Beatles', side1: ['Two of Us', 'Dig a Pony','Across the Universe','I Me Mine','Dig It','Let It Be','Maggie Mae'], side2: ['I\'ve Got a Feeling','One After 909','The Long And Winding Road','For You Blue','Get Back']}
 ];
